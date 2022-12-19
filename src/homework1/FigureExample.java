05028847667126975dc6f5bb69cfde3f5070b2bf
NazeliGhumashyan8719 <nazeli20331987@gmail.com>
2022-12-19 22:17:44 +0400

    add FigureExample

--- src/homework1/FigureExample.java
+++ src/homework1/FigureExample.java
@@ -1,10 +1,10 @@
+package homework1;
 
-public class Homework3 {
-
+public class FigureExample {
 
     public static void main(String[] args) {
-        for (int i = 5; i > 0; i--) {
-            for (int j = 0; j <= i; j++) {
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 5; j >= i; j--) {
                 System.out.print(" * ");
             }
             System.out.println();
