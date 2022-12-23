f5b708bba1c462f038b8f80f9d015700b2aa5d20
NazeliGhumashyan8719 <nazeli20331987@gmail.com>
2022-12-23 18:11:30 +0400

    add all homeworks

---
+++ src/homeworks/homework1/BoolExample.java
@@ -0,0 +1,31 @@
+package homeworks.homework1;
+
+public class BoolExample {
+
+    public static void main(String[] args) {
+        char a = 'h';
+        boolean b = a == 'h';
+        if (b == true) {
+
+            System.out.println(a);
+        if (b == false) {
+
+        }
+            System.out.println(b);
+        }
+
+    }
+
+    public static class CharExample {
+
+        public static void main(String[] args) {
+            char a = 'a';
+            a = 99;
+            System.out.println(a);
+            char b = 'b';
+            b = 82;
+            System.out.println(b);
+
+        }
+    }
+}
---
+++ src/homeworks/homework1/ForExample.java
@@ -0,0 +1,28 @@
+package homeworks.homework1;
+
+public class ForExample {
+
+    public static void main(String[] args) {
+
+       int a = 0;
+        int b = 7;
+        for (a = 0; a < 7; a++) {
+            System.out.print(" "+ a );
+        }
+    }
+
+    public static class IfExample {
+
+        public static void main(String[] args) {
+            int a, b;
+
+            a = 20;
+            b = 37;
+            if (a < b) {
+                System.out.println(b);
+            }
+
+
+        }
+    }
+}
