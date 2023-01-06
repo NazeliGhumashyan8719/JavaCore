1603a130b00d0448575e06fc797f3cee097f12ac
NazeliGhumashyan8719 <nazeli20331987@gmail.com>
2023-01-06 19:40:25 +0400

    add new changed ArrayUtil

--- src/homeworks/ArrayUtil.java
+++ src/homeworks/ArrayUtil.java
@@ -6,7 +6,7 @@ public class ArrayUtil {
         int[] array = {4, 7, 3, 12, 30, 16, 5, 11, 20, 8};
 
         for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
+            System.out.print(array[i] + " "); // tpel bolor elementner@
         }
         System.out.println();
 
@@ -16,9 +16,9 @@ public class ArrayUtil {
                 max = array[i];
             }
         }
-        System.out.println(max);
+        System.out.println(max);     // tpel max element@
 
-        int min = array[0];
+        int min = array[0];        // tpel min element@
         for (int i = 1; i < array.length; i++) {
             if (array[i] < min) {
                 min = array[i];
@@ -29,62 +29,53 @@ public class ArrayUtil {
         int i = array[0];
         for (int j = 0; j < array.length; j++) {
             if (array[j] % 2 == 0) {
-                System.out.print(array[j] + " ");
+                System.out.print(array[j] + " "); // tpel zujg elementner@
             }
         }
         System.out.println();
 
         for (int j = 0; j < array.length; j++) {
             if (array[j] % 2 == 1) {
-                System.out.print(array[j] + " ");
+                System.out.print(array[j] + " "); // tpel kent elementner@
             }
         }
         System.out.println();
-        System.out.print(array[0]);
+        System.out.print(array[0]); // tpel arajin element@
         System.out.println();
 
         int a = array.length - 1;
-        System.out.print(array[a]);
+        System.out.print(array[a]); // tpel verjin element@
 
         int b = 0;
         for (int j = 0; j < array.length; j++) {
             if (array[j] % 2 == 0) {
-                b = b + 1;
+                b = b + 1; // tpel zujg elementneri qanak@
             }
         }
         System.out.println();
         System.out.println(b);
 
         int k = 0;
-        for (int j = 3; j < array.length; j++) {
+        for (int j = 0; j < array.length; j++) {
             if (array[j] % 2 == 1) {
                 k = k + 1;
             }
         }
-        System.out.println(k);
+        System.out.println(k); // tpel kent elementneri qanak@
         System.out.println();
 
-        int h = array[0];
-        for (int j = 0; j < array.length; j++) {
-            if (array.length / 2 == h) {
-                h = array[j];
-                System.out.println(h);
-            }
-        }
         int c = 0;
         for (int j = 0; j < array.length; j++) {
             c = c + array[j];
         }
-        System.out.println(c / array.length);
+        System.out.println(c / array.length); //tpel mijin tvabanakan@
 
-        int d = array[0];
+        int d = 0; // tpel masivi elementneri gumar@
         for (int j = 0; j < array.length; j++) {
-            if (array.length / 2 == d) {
-                d = array[j];
-            }
+            d = d + array[j];
         }
         System.out.println(d);
-        System.out.println(array[array.length / 2]);
+        System.out.println(array[array.length / 2]); //tpel mejtexi tiv@
 
     }
 }
