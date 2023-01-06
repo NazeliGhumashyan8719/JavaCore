b9a06df17285b3028e22da3333c449d8b39d48e9
NazeliGhumashyan8719 <nazeli20331987@gmail.com>
2023-01-06 10:33:12 +0400

    add ArrayUtil

---
+++ src/chapter5/Break.java
@@ -0,0 +1,25 @@
+package chapter5;
+
+public class Break {
+
+    public static void main(String[] args) {  //формы оператора goto
+        boolean t = true;
+
+        first:
+        {
+            second:
+            {
+                third:
+                {
+                    System.out.println("Предшествует оператору break.");
+                    if (t) break second;// выход из блока second
+                    System.out.println("Этот оператор не будет выполняться");
+                }
+                System.out.println("Этот оператор не будет выполняться");
+            }
+            System.out.println("Этот оператор следует за блоком second.");
+        }
+    }
+}
+
+
---
+++ src/chapter5/Return.java
@@ -0,0 +1,14 @@
+package chapter5;
+
+public class Return {
+
+    public static void main(String[] args) {
+        boolean t = true;
+
+        System.out.println("До возврата.");
+
+        if (t) return; // возврат в вызывающий код
+
+        System.out.println("Этот оператор выполняться не будет.");
+    }
+}
