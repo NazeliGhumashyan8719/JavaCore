acea4509c76e5d2b9e587d4bd4238d3af1e05d2c
NazeliGhumashyan8719 <nazeli20331987@gmail.com>
2023-01-27 18:28:02 +0400

    add changed Commandline

---
+++ src/homework9/BraceChecker.java
@@ -0,0 +1,13 @@
+package homework9;
+
+public class BraceChecker {
+
+
+    private String text;
+    public BraceChecker(String text) {
+        this.text = text;
+
+        }
+
+    }
+
---
+++ src/homework9/BraceCheckerDemo.java
@@ -0,0 +1,9 @@
+package homework9;
+
+public class BraceCheckerDemo {
+
+    public static void main(String[] args) {
+        BraceChecker bc = new BraceChecker("Hello from [Java]");
+
+    }
+}
